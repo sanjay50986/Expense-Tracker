da2c7edc11e7ecddf08ee201540e0b2a622aeab7
Sanjay Jangid <sanjay50986@gmail.com>
2025-03-14 00:24:00 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './Pages/Auth/Login'
+import SignUp from './Pages/Auth/SignUp'
+import Home from './Pages/Dashboard/Home'
+import Income from './Pages/Dashboard/Income'
+import Expense from './Pages/Dashboard/Income'
+
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Root />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signUp' element={<Sign />} />
+          <Route path='/dashboard' element={<Home />} />
+          <Route path='/income' element={<Income />} />
+          <Route path='/expense' element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
+
+
+const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  )
+}
