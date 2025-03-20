4d04c45e6021762a7954e465df1b25b4e9124d4e
Sanjay Jangid <sanjay50986@gmail.com>
2025-03-21 00:59:17 +0530

    Signup page

--- Frontend/src/Pages/Auth/SignUp.jsx
+++ Frontend/src/Pages/Auth/SignUp.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthLayout from '../../Components/Layouts/AuthLayout'
+import Input from '../../Components/Inputs/Input'
+import ProfilePhotoSelector from '../../Components/Inputs/ProfilePhotoSelector'
+
 
 const SignUp = () => {
+
+  const [profilePic, setprofilePic] = useState(null)
+  const [fullName, setfullName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(null)
+
   return (
-    <div>
-      
-    </div>
+
+    <AuthLayout>
+      <div className='lg-w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center '>
+        <h3 className='text-xl font-semibold text-black'>Create an Account</h3>
+        <p className='text-xs text-slate-700 mt-[5px] mb-6'>Join us today by entering your details below.</p>
+
+        <form>
+
+          <ProfilePhotoSelector
+          image = {profilePic}
+          setImage={setprofilePic}/>
+
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+            <Input
+              value={fullName}
+              onChange={({ target }) => setfullName(target.value)}
+              label="Full Name"
+              placeholders="Jhon"
+              type='text' />
+
+            <Input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholders="jhon@example.com"
+              type='email' />
+
+            <div className="col-span-2">
+              <Input
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+                label="Password"
+                placeholders="Min 8 Characters"
+                type='password' />
+            </div>
+
+          </div>
+
+        </form>
+      </div>
+    </AuthLayout>
   )
 }
 
